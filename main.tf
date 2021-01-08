b822455e2f8170c093577cfdce234124fc58bb7a
Brandon Bird <brandon@yolabs.io>
2021-01-08 15:37:26 -0800

    actual code

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.0.0"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "random" {
+  length = 16
+  special = true
+  override_special = "/@£$"
+}
